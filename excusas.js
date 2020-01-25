5da42b7c4de7c78d0dcc0403bbfbbeba1fc813cb
jnaran02 <58963228+jnaran02@users.noreply.github.com>
2020-01-24 21:11:21 -0300

    Add files via upload

---
+++ excusas.js
@@ -0,0 +1,28 @@
+let quien = ["A Jose","Mi jefe","Mi perro","Un compañero","Un Conductor","Mi vecino"];
+let que = ["tenia diarrea","se murio","escuchaba rock a todo volumèn","se emborrachó","se suicidó", "se llevo mi tarea"];
+let cuando = ["el martes", "antes de ayer", "anoche", "mientras almorzaba", "al salir del trabajo","antes de dormir"];
+
+function aleatorio() {
+    
+    var numero = Math.floor((Math.random() * (6-1))+1);
+  
+return numero;
+};
+
+function concatenar() {
+     var excusa = {
+        indice: aleatorio(),
+        indice2:  aleatorio(),
+        indice3:  aleatorio(),
+     };
+
+     var cuento = quien[excusa.indice]+" "+que[excusa.indice2]+" "+cuando[excusa.indice3];
+     
+     document.getElementById('resultado').innerHTML = cuento;
+
+   //  return  historia;
+}
+
+
+
+
